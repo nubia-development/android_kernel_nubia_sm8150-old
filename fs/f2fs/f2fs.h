aa449736aa900ae9064a016776fc0ae348e9fc73
Cyborg2017 <wangyang0987654@gmail.com>
2019-12-30 12:56:50 +0800

    fs: Import nubia changes form nx627j_p_kernel

--- fs/f2fs/f2fs.h
+++ fs/f2fs/f2fs.h
@@ -1094,6 +1094,13 @@ enum fsync_mode {
 #define DUMMY_ENCRYPTION_ENABLED(sbi) (0)
 #endif
 
+#ifdef CONFIG_NUBIA_F2FS_TRIM_STAT
+#define NUBAI_F2FS_NO_TRIMED        0
+#define NUBAI_F2FS_TRIMING          1
+#define NUBAI_F2FS_TRIMED           2
+#define NUBIA_F2FS_EXIT_TRIM        9
+#endif
+
 struct f2fs_sb_info {
 	struct super_block *sb;			/* pointer to VFS super block */
 	struct proc_dir_entry *s_proc;		/* proc entry */
@@ -1262,6 +1269,9 @@ struct f2fs_sb_info {
 
 	/* Precomputed FS UUID checksum for seeding other checksums */
 	__u32 s_chksum_seed;
+#ifdef CONFIG_NUBIA_F2FS_TRIM_STAT
+    int trim_stat;        /*use for f2fs trim stats,no trimed,triming,trimed */
+#endif
 };
 
 #ifdef CONFIG_F2FS_FAULT_INJECTION
